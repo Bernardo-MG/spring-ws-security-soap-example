66bd8e7b4e85c8da6c740f9e1be6a9c24fc0bec6
Bernardo Martínez Garrido <programming@wandrell.com>
2016-03-04 14:30:04 +0100

    Endpoint URLs are now being correctly loaded in the client.

--- src/main/java/com/wandrell/example/swss/client/console/ConsoleClient.java
+++ src/main/java/com/wandrell/example/swss/client/console/ConsoleClient.java
@@ -190,28 +190,28 @@ public class ConsoleClient {
                 getEndpointUri("context/endpoint/endpoint-unsecure.properties"));
         uris.put(
                 PASSWORD_PLAIN_XWSS,
-                getEndpointUri("context/client/password/plain/xwss/client-password-plain-xwss.xml"));
+                getEndpointUri("context/endpoint/password/plain/xwss/endpoint-password-plain-xwss.properties"));
         uris.put(
                 PASSWORD_PLAIN_WSS4J,
-                getEndpointUri("context/client/password/plain/wss4j/client-password-plain-wss4j.xml"));
+                getEndpointUri("context/endpoint/password/plain/wss4j/endpoint-password-plain-wss4j.properties"));
         uris.put(
                 PASSWORD_DIGEST_XWSS,
-                getEndpointUri("context/client/password/digest/xwss/client-password-digest-xwss.xml"));
+                getEndpointUri("context/endpoint/password/digest/xwss/endpoint-password-digest-xwss.properties"));
         uris.put(
                 PASSWORD_DIGEST_WSS4J,
-                getEndpointUri("context/client/password/digest/wss4j/client-password-digest-wss4j.xml"));
+                getEndpointUri("context/endpoint/password/digest/wss4j/endpoint-password-digest-wss4j.properties"));
         uris.put(
                 SIGNATURE_XWSS,
-                getEndpointUri("context/client/signature/xwss/client-signature-xwss.xml"));
+                getEndpointUri("context/endpoint/signature/xwss/endpoint-signature-xwss.properties"));
         uris.put(
                 SIGNATURE_WSS4J,
-                getEndpointUri("context/client/signature/wss4j/client-signature-wss4j.xml"));
+                getEndpointUri("context/endpoint/signature/wss4j/endpoint-signature-wss4j.properties"));
         uris.put(
                 ENCRYPTION_XWSS,
-                getEndpointUri("context/client/encryption/xwss/client-encryption-xwss.xml"));
+                getEndpointUri("context/endpoint/encryption/xwss/endpoint-encryption-xwss.properties"));
         uris.put(
                 ENCRYPTION_WSS4J,
-                getEndpointUri("context/client/encryption/wss4j/client-encryption-wss4j.xml"));
+                getEndpointUri("context/endpoint/encryption/wss4j/endpoint-encryption-wss4j.properties"));
 
         return uris;
     }
@@ -220,6 +220,14 @@ public class ConsoleClient {
         output.println("Choose a client:");
         output.println();
         output.println("1.- Unsecure");
+        output.println("2.- Plain password (XWSS)");
+        output.println("3.- Plain password (WSS4J)");
+        output.println("4.- Digested password (XWSS)");
+        output.println("5.- Digested password (WSS4J)");
+        output.println("6.- Signature (XWSS)");
+        output.println("7.- Signature (WSS4J)");
+        output.println("8.- Encryption (XWSS)");
+        output.println("9.- Encryption (WSS4J)");
     }
 
     private static final void printHelp(final PrintStream output) {
@@ -253,6 +261,38 @@ public class ConsoleClient {
                 case "1":
                     client = clients.get(UNSECURE);
                     uri = uris.get(UNSECURE);
+                    break;
+                case "2":
+                    client = clients.get(PASSWORD_PLAIN_XWSS);
+                    uri = uris.get(PASSWORD_PLAIN_XWSS);
+                    break;
+                case "3":
+                    client = clients.get(PASSWORD_PLAIN_WSS4J);
+                    uri = uris.get(PASSWORD_PLAIN_WSS4J);
+                    break;
+                case "4":
+                    client = clients.get(PASSWORD_DIGEST_XWSS);
+                    uri = uris.get(PASSWORD_DIGEST_XWSS);
+                    break;
+                case "5":
+                    client = clients.get(PASSWORD_DIGEST_WSS4J);
+                    uri = uris.get(PASSWORD_DIGEST_WSS4J);
+                    break;
+                case "6":
+                    client = clients.get(SIGNATURE_XWSS);
+                    uri = uris.get(SIGNATURE_XWSS);
+                    break;
+                case "7":
+                    client = clients.get(SIGNATURE_WSS4J);
+                    uri = uris.get(SIGNATURE_WSS4J);
+                    break;
+                case "8":
+                    client = clients.get(ENCRYPTION_XWSS);
+                    uri = uris.get(ENCRYPTION_XWSS);
+                    break;
+                case "9":
+                    client = clients.get(ENCRYPTION_WSS4J);
+                    uri = uris.get(ENCRYPTION_WSS4J);
                     break;
                 default:
                     client = null;
