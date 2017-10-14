a0dda5bd162dac6330852a77f22ada5a0632c107
Bernardo <programming@wandrell.com>
2017-10-14 13:01:49 +0200

    Using Liquibase for DB versioning

--- src/main/resources/db/populateDB.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO example_entities (id, name) VALUES (1, 'entity_1');
-INSERT INTO example_entities (id, name) VALUES (2, 'entity_2');
-INSERT INTO example_entities (id, name) VALUES (3, 'entity_3');
-INSERT INTO example_entities (id, name) VALUES (4, 'entity_4');
